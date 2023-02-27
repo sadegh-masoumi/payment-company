a98c476c5a13fd35627265158d4705bf89f76d25
mohammad sadegh masoumi <sadegh.masoumi.ms@gmail.com>
2023-02-27 20:49:23 +0330

    [startapp]

---
+++ db/database.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+
+def get_db_handle(db_name: str, host: str, port: int, username: str, password: str):
+    client = MongoClient(
+        host=host, port=int(port), username=username, password=password
+    )
+    db_handle = client[db_name]
+    return db_handle, client
